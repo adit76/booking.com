c18e73845c7994620d040caf7f8ece1c7369098e
adit76 <aditsapkota.as@gmail.com>
2022-08-11 00:41:12 +0545

    Data Restructure and Updates for Filter Functionlity, state moved to data file filterOption and guestsLists

---
+++ src/data/guestsLists.js
@@ -0,0 +1,54 @@
+import { v4 as uuidv4 } from 'uuid';
+
+const guestsList = [
+    {
+        guestName: "S Kraswo",
+        noOfGuests: 2,
+        checkIn: "13 Jul 2022",
+        checkout: "16 Jul 2022",
+        room: "Single Room",
+        bookedOn: "10 Jul 2022",
+        status: "Ok",
+        price: 135,
+        commission: 16.20,
+        bookingNumber: uuidv4()
+    },
+    {
+        guestName: "S Kraswo",
+        noOfGuests: 2,
+        checkIn: "13 Jul 2022",
+        checkout: "16 Jul 2022",
+        room: "Single Room",
+        bookedOn: "10 Jul 2022",
+        status: "Ok",
+        price: 135,
+        commission: 16.20,
+        bookingNumber: uuidv4()
+    },
+    {
+        guestName: "S Kraswo",
+        noOfGuests: 2,
+        checkIn: "13 Jul 2022",
+        checkout: "16 Jul 2022",
+        room: "Single Room",
+        bookedOn: "10 Jul 2022",
+        status: "Ok",
+        price: 135,
+        commission: 16.20,
+        bookingNumber: uuidv4()
+    },
+    {
+        guestName: "S Kraswo",
+        noOfGuests: 2,
+        checkIn: "13 Jul 2022",
+        checkout: "16 Jul 2022",
+        room: "Single Room",
+        bookedOn: "10 Jul 2022",
+        status: "Ok",
+        price: 135,
+        commission: 16.20,
+        bookingNumber: uuidv4()
+    },
+]
+
+export default guestsList;
